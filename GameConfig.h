fcf91104d0e717f81dfe0fbeef829efe0d16f9d5
Heim <lucas.heim@sap.com>
2018-11-26 18:45:36 -0200

    Removing constants from main

--- GameConfig.h
+++ GameConfig.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#define APPLICATION_NAME "Space Invaders"
+#define FRAGMENT_SHADER_PATH "fragment_shader.fs"
+#define VERTEX_SHADER_PATH "vertex_shader.vs"
 #define GAME_MAX_BULLETS 128
 #define NUM_ALIENS 55
 #define PLAYER_LIFES 3
